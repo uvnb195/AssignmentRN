b8fa59f25f7acf25d249bd9d09c9c39c0e330dff
Quann <uvnb195@gmail.com>
2024-02-23 18:09:35 +0700

    create assignmentJs folder

--- AssignmentRN/src/constants/index.tsx
+++ AssignmentRN/src/constants/index.tsx
@@ -1,6 +1,6 @@
 export type KeyboardType = 'default' | 'email-address' | 'number-pad';
 
-export const BASE_URL = 'http://10.0.105.142:3070'
+export const BASE_URL = 'http://172.16.48.157:3070'
 
 // export const LOGGER = 
 
